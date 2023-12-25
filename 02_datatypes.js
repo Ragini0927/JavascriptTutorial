9f8ea649b386199fbf2a434aa5640b16b8342247
Ragini0927 <myselfraginisingh@gmail.com>
2023-12-25 13:34:16 +0530

    Datatpes in Javascript

---
+++ 02_datatypes.js
@@ -0,0 +1,22 @@
+ "use strict";//treat all js code as newer version
+
+//  alert(3+3) // we are using node js and not browser
+console.log(3+3)
+
+let name="Ragini"
+let age =18
+let loggedIn=false
+let x
+let y=null
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null =>standalone value
+// undefined => when value is not assigned
+// symbol => unique
+
+// Object
+console.log(typeof age)
+console.log(typeof x)
+console.log(typeof y)
