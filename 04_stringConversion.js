4dcbdda1bc254b162e327b97136683d8db8e2fc4
Ragini0927 <myselfraginisingh@gmail.com>
2023-12-26 12:26:18 +0530

    String Conversin

---
+++ 04_stringConversion.js
@@ -0,0 +1,43 @@
+let value=3
+let negValue=-value;
+console.log(negValue)
+// console.log(2*2)
+// console.log(2+2)
+// console.log(3%2)
+// console.log(6/2)
+// console.log(2**2)
+
+
+let str1="hello"
+let str2=" world"
+let str3=str1+str2
+console.log(str3)
+
+console.log("1"+2) //12
+console.log(1+"2") //12
+console.log("1"+2+2) //122
+console.log(1+2+"2") //32
+//  if string comes first then every number will be treated as string and concatenate
+// if number comes first then the regular operation will be performed and will be 
+ 
+console.log(+true) //1
+console.log(+"") //0
+// console.log(true+) error
+
+let num1,num2,num3
+num1=num2=num3=2+2
+console.log(num1)
+
+let gameCounter1=1;
+let y=gameCounter1++; //y==1 and gameCounter=2
+
+let gameCounter2=1
+let x=++gameCounter2 //x=2 and gameCounter=2
+// postfix =>increase the given value but return the original value
+//  prefix =>increment ad return the incremented value
+console.log(gameCounter1)
+console.log(y)
+
+console.log(x)
+console.log(gameCounter2)
+
