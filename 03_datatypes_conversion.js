7ff256357fb22ae7cabb13232c4795cbcaaf0c1c
Ragini0927 <myselfraginisingh@gmail.com>
2023-12-26 11:46:09 +0530

    DataTypes Conversion

---
+++ 03_datatypes_conversion.js
@@ -0,0 +1,32 @@
+let score ="33"
+console.log(typeof score);
+console.log(typeof(score));
+
+let inNumber=Number(score)
+console.log(typeof inNumber);
+console.log(inNumber)
+
+
+let id="12ab"
+console.log(typeof id)
+console.log(id)
+let numberId=Number(id)
+console.log(typeof numberId)
+console.log(numberId)
+
+// "33" =>33
+// "33abc" =>NaN
+// true => 1; false =>0
+
+let isLoggedIn="1"
+let booleanLoggedIn=Boolean(isLoggedIn)
+console.log(booleanLoggedIn)
+
+// 1 =>true; 0=>false
+//  "" =>false (Empty String)
+// "Ragini" =>true 
+
+
+let x=33
+let y=String(x)
+console.log(typeof y)
